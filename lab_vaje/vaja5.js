cc55d728884f4d80a2c924a26a3b81432c1c55e8
dejan <dejan@meznarc.si>
2022-11-09 14:46:53 +0100

    prev

--- lab_vaje/vaja5.js
+++ lab_vaje/vaja5.js
@@ -1,13 +1,92 @@
+var gameState = [[0, 0], [0, 0]]; // 6x6 arr - [row][col]
 
-f0 = 1
-f1 = 1
-f2 = f1+f0;
+const GAME_WIN_THRESHOLD = 4;
 
-
-for (let i = 0; i < 10; i++) {
-    f1 = f2;
-    f2 = (f2-f1) + f1;
-
+function gameStateReset() {
+    gameState = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+    ];
 }
 
-console.log(f2);
+gameStateReset();
+console.log(gameState);
+
+function gameStateRender() {
+    for (let row = 0; row < gameState.length; row++) {
+        rowf = "";
+        for (let col = 0; col < gameState[0].length; col++) {
+            rowf += gameState[row][col] + " ";
+        }
+        console.log(rowf);
+    }
+}
+
+function gamePutIn(playerId, colNum) {
+    for (let row = gameState.length - 1; row >= 0; row--) {
+        if (gameState[row][colNum] === 0) {
+            gameState[row][colNum] = playerId;
+            break;
+        }
+    }
+}
+
+
+function getGameWinner() {
+    // check every row
+    for (let row = 0; row < gameState.length; row++) {
+        lastPlayerId = 0;
+        count = 0;
+        for (let col = 0; col < gameState[0].length; col++) {
+            if (gameState[row][col] > 0) {
+                count = (gameState[row][col] === lastPlayerId) ? count + 1 : 0;
+                if(count >= GAME_WIN_THRESHOLD) return lastPlayerId;
+            }
+            lastPlayerId = gameState[row][col];
+        }
+    }
+
+    // check every col
+
+
+    for (let col = 0; col < gameState.length; col++) {
+        lastPlayerId = 0;
+        count = 0;
+        for (let row = 0; row < gameState[0].length; row++) {
+            if (gameState[col][row] > 0) {
+                count = (gameState[col][row] === lastPlayerId) ? count + 1 : 0;
+                if(count >= GAME_WIN_THRESHOLD) return lastPlayerId;
+            }
+            lastPlayerId = gameState[col][row];
+        }
+    }
+}
+
+
+gamePutIn(1, 1);
+gamePutIn(2, 3);
+gamePutIn(2, 3);
+gamePutIn(2, 3);
+gamePutIn(2, 3);
+
+gameStateRender();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
